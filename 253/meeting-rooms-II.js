1c8a8bb5ae50212fb6096519c55fadc6dade2238
Victor Hugo Chimenez <victorhq.inbox@gmail.com>
2024-04-23 10:53:35 -0300

    feat: solve leet code 253

--- 253/meeting-rooms-II.js
+++ 253/meeting-rooms-II.js
@@ -1,49 +1,12 @@
 /*
 Runtime
     44ms
-    Beats 98.94% of users with JavaScript
+    Beats 50.23% of users with JavaScript
 
 Memory
     49.15MB
-    Beats 91.48% of users with JavaScript
+    Beats 85.67% of users with JavaScript
 */
-
-class Rooms {
-    constructor() {
-        this.rooms = [new Map()];
-    }
-
-    size() {
-        return this.rooms.length;
-    }
-
-    getAvailableRoom() {
-        return this.rooms.indexOf(this.rooms[this.rooms.length - 1])
-    }
-
-    createNewRoom() {
-        const newRoom = new Map();
-        this.rooms.push(newRoom);
-        return this.rooms.indexOf(newRoom);
-    }
-
-    isRoomAvailable(startTime, endTime, roomIndex) {
-        const room = this.rooms[roomIndex]
-        for (let i = startTime; i <= endTime; i++) {
-            if (room.has(i)) {
-                return false
-            }
-        }
-        return true
-    }
-
-    reserveRoom(startTime, endTime, roomIndex) {
-        const room = this.rooms[roomIndex]
-        for (let i = startTime; i <= endTime-1; i++) {
-            room.set(i, true)
-        }
-    }
-}
 
 /**
  * @param {number[][]} intervals
@@ -61,7 +24,6 @@ function minMeetingRooms(intervals) {
 
     // Função para ajustar o heap (emular um min-heap com array)
     function minHeapify() {
-        console.log('endTimes', endTimes)
         endTimes.sort((a, b) => a - b);
     }
 
